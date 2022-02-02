2dd6245f068e05b7638615ba29bf9ccb3ae682ce
Documenter.jl <documenter@juliadocs.github.io>
2022-02-02 11:31:54 +0000

    build based on b9bf732

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PyPlotly","category":"page"},{"location":"#PyPlotly","page":"Home","title":"PyPlotly","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PyPlotly.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PyPlotly]","category":"page"}]
+}
